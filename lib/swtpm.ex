1887637def929014398772b8d6696ae79495ddc2
abelino <abelino.romo@gmail.com>
2024-12-19 11:19:43 -0800

    Fix dialyzer error

--- lib/swtpm.ex
+++ lib/swtpm.ex
@@ -29,7 +29,7 @@ defmodule SWTPM do
   Supervisor.start_link(children, [strategy: :one_for_one, name: MySupervisor])
   ```
   """
-  @spec child_spec(term) :: Supervisor.child_spec()
+  @spec child_spec(keyword) :: Supervisor.child_spec()
   def child_spec(opts) do
     state_dir   = opts[:state_dir]
     server_port = opts[:server_port] || 2321
@@ -40,16 +40,21 @@ defmodule SWTPM do
       |> Enum.map(& to_string(&1) |> String.replace("_", "-"))
       |> Enum.join(",")
 
-    MuonTrap.Daemon.child_spec([
-      Path.join([:code.priv_dir(:swtpm), "bin", "swtpm"]),
-      [
-        "socket",
-        "--tpm2",
-        "--tpmstate", "dir=#{state_dir}",
-        "--server",   "type=tcp,port=#{server_port}",
-        "--ctrl",     "type=tcp,port=#{ctrl_port}",
-        "--flags",    flags
-      ]
-    ])
+    command = Path.join([:code.priv_dir(:swtpm), "bin", "swtpm"])
+    args = [
+      "socket",
+      "--tpm2",
+      "--tpmstate", "dir=#{state_dir}",
+      "--server",   "type=tcp,port=#{server_port}",
+      "--ctrl",     "type=tcp,port=#{ctrl_port}",
+      "--flags",    flags,
+    ]
+
+    %{
+      id:      __MODULE__,
+      start:   {MuonTrap.Daemon, :start_link, [command, args, []]},
+      type:    :worker,
+      restart: :permanent,
+    }
   end
 end
